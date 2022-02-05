e24926ccd75b6177d35c552006d961d66044f0cc
ubuntu <ubuntu@localhost.localdomain>
2022-02-05 09:23:48 +0800

    Upgrade to v0.9.16

--- zenlink-protocol/src/lib.rs
+++ zenlink-protocol/src/lib.rs
@@ -99,6 +99,7 @@ pub mod pallet {
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
+	#[pallet::without_storage_info]
 	pub struct Pallet<T>(_);
 
 	/// Foreign foreign storage
